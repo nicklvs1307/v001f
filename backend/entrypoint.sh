9e8d33f6de21b3ebf5e239ee9d455e76bf81c2f9
Lucas <lucas.niquele.ln@gmail.com>
2025-09-25 12:53:44 -0300

    feat: atualiza projeto feedeliza

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,16 +2,7 @@
 # O comando 'set -e' garante que o script irá parar se algum comando falhar.
 set -e
 
-echo "Aguardando o banco de dados ficar pronto..."
-# Adicionar um pequeno delay pode ajudar a garantir que o DB esteja pronto para aceitar conexões.
-# Para uma solução mais robusta, um script como 'wait-for-it.sh' seria ideal.
-sleep 5
-
-echo "Rodando as migrações do banco de dados..."
-# Executa o comando de migração do Sequelize
-npx sequelize-cli db:migrate
-
-echo "Migrações concluídas. Iniciando a aplicação..."
+echo "Iniciando a aplicação..."
 # O comando 'exec "$@"' executa o comando que foi passado como argumento para o entrypoint.
 # No nosso caso, será o CMD do Dockerfile: ["node", "src/server.js"]
 exec "$@"

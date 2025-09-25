d5373a02c9f37859a7bafc0436fc63a4cb629c69
Lucas <lucas.niquele.ln@gmail.com>
2025-09-25 19:10:02 -0300

    feat: atualiza projeto feedeliza

--- backend/src/config/sequelize-config.js
+++ backend/src/config/sequelize-config.js
@@ -1,7 +1,14 @@
 require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
 
 module.exports = {
-    "password": process.env.DB_PASSWORD || (process.env.DB_PASSWORD_FILE ? require('fs').readFileSync(process.env.DB_PASSWORD_FILE, 'utf8').trim() : null),,
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD || (process.env.DB_PASSWORD_FILE ? require('fs').readFileSync(process.env.DB_PASSWORD_FILE, 'utf8').trim() : null),
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "postgres"
+  },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD || (process.env.DB_PASSWORD_FILE ? require('fs').readFileSync(process.env.DB_PASSWORD_FILE, 'utf8').trim() : null),

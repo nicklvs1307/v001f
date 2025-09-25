a7b84410717e9c319918a2711f5f72d80d987fb9
Lucas <lucas.niquele.ln@gmail.com>
2025-09-25 19:07:50 -0300

    feat: atualiza projeto feedeliza

--- backend/src/config/sequelize-config.js
+++ backend/src/config/sequelize-config.js
@@ -1,17 +1,10 @@
 require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": "postgres"
-  },
+    "password": process.env.DB_PASSWORD || (process.env.DB_PASSWORD_FILE ? require('fs').readFileSync(process.env.DB_PASSWORD_FILE, 'utf8').trim() : null),,
   "test": {
     "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
+    "password": process.env.DB_PASSWORD || (process.env.DB_PASSWORD_FILE ? require('fs').readFileSync(process.env.DB_PASSWORD_FILE, 'utf8').trim() : null),
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
@@ -19,7 +12,7 @@ module.exports = {
   },
   "production": {
     "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
+    "password": process.env.DB_PASSWORD || (process.env.DB_PASSWORD_FILE ? require('fs').readFileSync(process.env.DB_PASSWORD_FILE, 'utf8').trim() : null),
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,

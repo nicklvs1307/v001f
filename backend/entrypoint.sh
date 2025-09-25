3c432e549e3460b048bab3f65c6be9d6f8f389f3
Lucas <lucas.niquele.ln@gmail.com>
2025-09-25 16:15:27 -0300

    refactor: Unifica e otimiza configuração Docker

--- backend/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# O comando 'set -e' garante que o script irá parar se algum comando falhar.
-set -e
-
-echo "Iniciando a aplicação..."
-# O comando 'exec "$@"' executa o comando que foi passado como argumento para o entrypoint.
-# No nosso caso, será o CMD do Dockerfile: ["node", "src/server.js"]
-exec "$@"

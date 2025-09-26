a2cff19889d616b7a4e5b1244bbccc869a6e5f0e
Lucas <lucas.niquele.ln@gmail.com>
2025-09-26 08:23:16 -0300

    feat: atualiza projeto feedeliza

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -32,6 +32,6 @@ router.post(
 );
 
 // Rota para verificar o token
-router.post("/verify-token", authController.verifyToken);
+router.post("/verify-token", protect, authController.verifyToken);
 
 module.exports = router;

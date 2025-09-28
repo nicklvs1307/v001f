6325e3d139809153914dab46e6411ecb04b47511
Lucas <lucas.niquele.ln@gmail.com>
2025-09-28 17:31:08 -0300

    feat: atualiza projeto feedeliza

---
+++ backend/src/repositories/roletaSpinRepository.js
@@ -0,0 +1,9 @@
+const { RoletaSpin } = require('../../models');
+
+class RoletaSpinRepository {
+  async bulkCreate(data) {
+    return RoletaSpin.bulkCreate(data);
+  }
+}
+
+module.exports = new RoletaSpinRepository();

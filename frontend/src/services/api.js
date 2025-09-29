f3e812537c8d126eb26a50dae949f6526dedce4e
Lucas <lucas.niquele.ln@gmail.com>
2025-09-28 22:00:25 -0300

    feat: atualiza projeto feedeliza

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 // Cria a instância base do Axios.
 // Esta instância não deve ter interceptors de autenticação.
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api',
-
+  baseURL: `${process.env.REACT_APP_API_URL || ''}/api`,
 });
 
 export default api;

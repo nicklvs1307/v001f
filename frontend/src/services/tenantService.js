76358b95b46a7d14790f995cf48adae7ef20ee5f
Lucas <lucas.niquele.ln@gmail.com>
2025-10-14 11:38:33 -0300

    feat: atualiza projeto feedeliza

--- frontend/src/services/tenantService.js
+++ frontend/src/services/tenantService.js
@@ -3,6 +3,9 @@ import apiAuthenticated from './apiAuthenticated';
 const tenantService = {
   getMe: () => apiAuthenticated.get('/tenants/me'),
   update: (data) => apiAuthenticated.put('/tenants/me', data),
+  getAllTenants: () => apiAuthenticated.get('/tenants'),
 };
+
+export default tenantService;
 
 export default tenantService;

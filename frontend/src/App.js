50f45f1ee920d3ed6643aecd94cf655bbfc7be95
Lucas <lucas.niquele.ln@gmail.com>
2025-11-05 11:13:20 -0300

    feat: atualizaçao de quebra codigo, notificações push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CustomThemeProvider } from './context/ThemeContext';
+import { NotificationProvider } from './context/NotificationContext';
 import AppRoutes from './routes/AppRoutes'; // Importar o novo componente de rotas
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <Router>
       <AuthProvider>
             <CustomThemeProvider>
-              <AppRoutes /> {/* Usar o componente AppRoutes */}
+              <NotificationProvider>
+                <AppRoutes /> {/* Usar o componente AppRoutes */}
+              </NotificationProvider>
             </CustomThemeProvider>
       </AuthProvider>
     </Router>

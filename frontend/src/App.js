0dd8054cf927fa8d848cd687315ca9cf1c0340e8
Lucas <lucas.niquele.ln@gmail.com>
2025-11-05 10:19:58 -0300

    feat: atualização complementar para o voltaki - implantação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CustomThemeProvider } from './context/ThemeContext';
-import { SocketProvider } from './context/SocketContext';
-import { NotificationsProvider } from './context/NotificationsContext';
 import AppRoutes from './routes/AppRoutes'; // Importar o novo componente de rotas
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <SocketProvider>
-          <NotificationsProvider>
             <CustomThemeProvider>
               <AppRoutes /> {/* Usar o componente AppRoutes */}
             </CustomThemeProvider>
-          </NotificationsProvider>
-        </SocketProvider>
       </AuthProvider>
     </Router>
   );

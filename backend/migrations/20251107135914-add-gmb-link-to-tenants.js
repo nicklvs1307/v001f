25a42bdcacf18724d3658f119bf61ce3f6c42ff7
Lucas <lucas.niquele.ln@gmail.com>
2025-11-07 13:14:18 -0300

    feat: atualizaçao de quebra codigo, notificações push

--- backend/migrations/20251107135914-add-gmb-link-to-tenants.js
+++ backend/migrations/20251107135914-add-gmb-link-to-tenants.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Tenants', 'gmb_link', {
+    await queryInterface.addColumn('tenants', 'gmb_link', {
       type: Sequelize.STRING,
       allowNull: true,
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Tenants', 'gmb_link');
+    await queryInterface.removeColumn('tenants', 'gmb_link');
   },
 };

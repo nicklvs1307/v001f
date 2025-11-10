0aa9de14e15a48be952000772ef7c64f251cdb46
Lucas <lucas.niquele.ln@gmail.com>
2025-11-10 12:58:22 -0300

    feat: atualizações gerais

--- frontend/src/pages/CampaignFormPage.js
+++ frontend/src/pages/CampaignFormPage.js
@@ -29,6 +29,7 @@ import {
   DialogContentText,
   DialogActions
 } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import FormatBoldIcon from '@mui/icons-material/FormatBold';

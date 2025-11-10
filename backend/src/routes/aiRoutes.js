e2d2f6f6a677aacbce1ee8f5e1e524659b800a8a
Lucas <lucas.niquele.ln@gmail.com>
2025-11-10 10:49:49 -0300

    feat: atualizações gerais

--- backend/src/routes/aiRoutes.js
+++ backend/src/routes/aiRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const AiController = require('../controllers/aiController');
+const aiController = require('../controllers/aiController');
 const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
-const aiController = new AiController();
 
 // All routes in this file are protected
 router.use(protect);

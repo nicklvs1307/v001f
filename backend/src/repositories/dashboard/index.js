c92e2f31ae71ec8df090d958ccf0c7f41e7bc191
Lucas <lucas.niquele.ln@gmail.com>
2025-11-19 09:30:11 -0300

    feat: atuailizações relatorios

--- backend/src/repositories/dashboard/index.js
+++ backend/src/repositories/dashboard/index.js
@@ -1,11 +1,14 @@
 const { getMainDashboard } = require("./main");
-const { getDetails, getResponseDetailsBySessionId: getResponseDetails } = require("./details");
+const {
+  getDetailsByCategory,
+  getResponseDetailsBySessionId: getResponseDetails,
+} = require("./details");
 const { getAttendantDetails } = require("./attendants");
 const { getMonthlySummary: getMonthSummary } = require("./summary");
 
 module.exports = {
   getMainDashboard,
-  getDetails,
+  getDetailsByCategory,
   getAttendantDetails,
   getResponseDetails,
   getMonthSummary,

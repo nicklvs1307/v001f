061e32fd6c9bd7fe499eff81000a4adc391dab71
Lucas <lucas.niquele.ln@gmail.com>
2025-11-23 11:20:53 -0300

    feat:atualizaçao envio de mensagem

--- backend/src/routes/roletaRoutes.js
+++ backend/src/routes/roletaRoutes.js
@@ -30,6 +30,5 @@ router.get("/config/:pesquisaId/:clientId", roletaController.getRoletaConfig);
 router.get("/spins/validate/:token", roletaSpinController.validateToken);
 router.post("/spins/spin/:token", roletaSpinController.spinRoleta);
 
-router.post("/send-prize-message", roletaController.sendPrizeMessage);
 
 module.exports = router;
